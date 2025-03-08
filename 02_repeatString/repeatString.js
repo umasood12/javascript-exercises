d4f38110bc2a91adf4934ceacf04fdb33b7c4a45
umasood12 <umasood@utexas.edu>
2025-03-08 15:27:14 -0500

    finished first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let string = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (i = 0; i < num; i++) {
+            string += word;
+            console.log(word);
+        }
+        return string;
+    }
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
