85e69d29a3254344b9293c713718ebb100361188
umasood12 <umasood@utexas.edu>
2025-03-08 15:37:48 -0500

    finished exercise 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = '';
+    for (let i = string.length; i > 0; i--) {
+        reverseString += string[i - 1];
+    }
+    return reverseString; 
 };
 
+console.log(reverseString('test'));
 // Do not edit below this line
 module.exports = reverseString;
