99c6a09c44ffbdca1f397e05ff6b91149746f0d3
umasood12 <umasood@utexas.edu>
2025-03-09 16:12:36 -0400

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,28 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-
-    for (let i = num1; i <= num2; i++) {
-        sum += i;
+    
+    // CHECK IF NUMBER IS POSITIVE
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (Number.isInteger(num1) == false || Number.isInteger(num2) == false) {
+        return "ERROR";
     }
-    return sum;
+    
+    if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        for ( let i = num1; i <= num2; i++) {
+            sum +=i; 
+        }
+        return sum;
+    }
 };
 
-console.log(sumAll(2, 12));
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
