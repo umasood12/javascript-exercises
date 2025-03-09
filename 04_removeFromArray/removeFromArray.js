6a03d41587fdec24756d711458570a0346e0ef41
umasood12 <umasood@utexas.edu>
2025-03-09 15:39:33 -0400

    Finished Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+
+    // check if each 
+    for (i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i]) == false) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+console.log(removeFromArray([5,3,5], 3, 2, 9));
 
 // Do not edit below this line
 module.exports = removeFromArray;
