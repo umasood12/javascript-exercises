ff3a55e6000d4ad0afa062625527953874ebad5a
umasood12 <umasood@utexas.edu>
2025-03-10 13:55:42 -0400

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  return Math.round((ftemp - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  return Math.round(((ctemp * 9) / 5 + 32) * 10) / 10;
 };
+
+console.log("Temperatore: ", convertToCelsius(32));
+console.log("Temperatore: ", convertToCelsius(100));
+console.log("Temperatore: ", convertToCelsius(-100));
+
 
 // Do not edit below this line
 module.exports = {
